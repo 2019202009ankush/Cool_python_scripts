741f3e2899ea2b5268849c2fe3f3980c657c6af7
Ankush <ankush.mitra@students.iiit.ac.in>
2019-11-06 00:49:58 +0530

    added updated version of snake game

--- new_snake_game.py
+++ new_snake_game.py
@@ -1,96 +1,82 @@
 import random
 import curses
 from curses import textpad
-
-def create_food(snake, box):
-    '''Function to create Food'''
-
-    food = None
-    while food is None:
-        food = [random.randint(box[0][0]+1, box[1][0]-1),
-        random.randint(box[0][1]+1, box[1][1]-1)]
-
-        if food in snake:
-            food = None
-
-    return food
-
-
-def print_score(stdscr, score):
-    '''Function to print score'''
-
-    screen_height, screen_width = stdscr.getmaxyx()
-    score_text = "score.{}".format(score)
-    stdscr.addstr(0, screen_width//2 - len(score_text)//2, score_text)
+def make_suitable_screen(stdscr):
+    curses.init_color(curses.COLOR_BLACK, 0, 0, 0)
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_RED)
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_GREEN)
+    curses.curs_set(0)
+    stdscr.nodelay(1)
+    stdscr.timeout(200)
+def make_frog_cordinate(python_snake, game_board):
+    frog = None
+    while frog is None:
+        frog = [random.randint(game_board[0][0]+1, game_board[1][0]-1),random.randint(game_board[0][1]+1, game_board[1][1]-1)]
+        if frog in python_snake:
+            frog = None
+    return frog
+def print_score(stdscr, point,level):
+    curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    temp=level
+    sh,sw = stdscr.getmaxyx()
+    point_txt = "Point={}".format(point)
+    level_txt = "level={}".format(level)
+    stdscr.addstr(0, sw//2 - len(point_txt)//2, point_txt, curses.color_pair(3))
+    stdscr.addstr(0, (sw//2 - len(point_txt)//2)+10, level_txt, curses.color_pair(4))
+    stdscr.timeout(200-(20*temp))
     stdscr.refresh()
-
-
 def main(stdscr):
-
-    curses.curs_set(0)  # deactivate curser blinking
-    stdscr.nodelay(1)  # getch() is non blocking
-    stdscr.timeout(150) # how much time to wait for user response in ms
-
-    # fetching screen height and width
-    screen_height, screen_width = stdscr.getmaxyx()
-
-    box = [[3,3], [screen_height-3, screen_width-3]]
-    textpad.rectangle(stdscr, box[0][0], box[0][1], box[1][0], box[1][1])
-
-    """Initialising snake"""
-    snake = [[screen_height//2, screen_width//2+1], [screen_height//2, screen_width//2], [screen_height//2, screen_width//2-1]]
-    direction = curses.KEY_RIGHT
-
-    for y, x in snake:
-        stdscr.addstr(y, x, '#')
-
-    food = create_food(snake, box)
-    stdscr.addstr(food[0], food[1], '*')
-
-    score = 0
-    print_score(stdscr, score)
-
-    """defining movements"""
+    make_suitable_screen(stdscr)
+    sh, sw = stdscr.getmaxyx()
+    game_board = [[1,1], [sh-3, sw-3]]
+    textpad.rectangle(stdscr, game_board[0][0], game_board[0][1], game_board[1][0], game_board[1][1])
+    python_snake = [[sh//2, sw//2+1], [sh//2, sw//2], [sh//2, sw//2-1],[sh//2, sw//2-2]]
+    direction = curses.KEY_UP
+    for y, x in python_snake:
+        stdscr.addstr(y, x, '$', curses.color_pair(1))
+    frog = make_frog_cordinate(python_snake, game_board)
+    stdscr.addstr(frog[0], frog[1], '^',curses.color_pair(2))
+    point = 0
+    level=1
+    print_score(stdscr, point,level)
     while 1:
         key = stdscr.getch()
         if key in [curses.KEY_RIGHT, curses.KEY_LEFT, curses.KEY_UP, curses.KEY_DOWN]:
             direction = key
-
-        head = snake[0]
+        front = python_snake[0]
         if direction == curses.KEY_RIGHT:
-            new_head = [head[0], head[1]+1] #y, x axis
+            new_head = [front[0], front[1]+1]
         elif direction == curses.KEY_LEFT:
-            new_head = [head[0], head[1]-1]
+            new_head = [front[0], front[1]-1]
         if direction == curses.KEY_UP:
-            new_head = [head[0]-1, head[1]]
+            new_head = [front[0]-1, front[1]]
         if direction == curses.KEY_DOWN:
-            new_head = [head[0]+1, head[1]]
-
-        snake.insert(0, new_head)
-        stdscr.addstr(new_head[0], new_head[1], '#')
-
-
-        if snake[0] == food:
-            food = create_food(snake, box)
-            stdscr.addstr(food[0], food[1], '*')
-
-            score += 1
-            print_score(stdscr, score)
+            new_head = [front[0]+1, front[1]]
+        python_snake.insert(0, new_head)
+        stdscr.addstr(new_head[0], new_head[1], '$', curses.color_pair(1))
+        if python_snake[0] == frog:
+            frog = make_frog_cordinate(python_snake, game_board)
+            stdscr.addstr(frog[0], frog[1], '^',curses.color_pair(2))
+            point += 1
+            if point % 3 == 0 :
+                level+=1
+            print_score(stdscr, point,level)
+            if level == 4:
+                string_ = "YOU WIN"
+                stdscr.addstr(sh//2, sw//2-len(string_)//2, string_)
+                stdscr.nodelay(0)
+                stdscr.getch()
+                exit()
         else:
-            stdscr.addstr(snake[-1][0], snake[-1][1], ' ')
-            snake.pop()
-
-        if (snake[0][0] in [box[0][0], box[1][0]] or
-            snake[0][1] in [box[0][1], box[1][1]] or
-            snake[0] in snake[1:]
-        ):
-            msg = "GameOver!"
-            stdscr.addstr(screen_height//2, screen_width//2-len(msg)//2, msg)
+            stdscr.addstr(python_snake[-1][0], python_snake[-1][1], ' ')
+            python_snake.pop()
+        if (python_snake[0][0] in [game_board[0][0], game_board[1][0]] or python_snake[0][1] in [game_board[0][1], game_board[1][1]] or python_snake[0] in python_snake[1:]):
+            string_ = "Snake_Crashed"
+            stdscr.addstr(sh//2, sw//2-len(string_)//2, string_)
             stdscr.nodelay(0)
             stdscr.getch()
             break
-
         stdscr.refresh()
-
     stdscr.getch()
 curses.wrapper(main)
